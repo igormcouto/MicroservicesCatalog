30300ff8d4f44021cdb0097cb7ed14bfe01441d6
igormcouto <52253601+igormcouto@users.noreply.github.com>
2022-04-29 09:12:46 -0300

    Add project files.

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,97 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public CatalogController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name ="GetProduct")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProduct(id);
+
+            if (product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [Route("[action]/{category}",Name ="GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public  async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            if (category is null)
+                return BadRequest("Invalid category");
+
+            var product = await _repository.GetProductByCategory(category);
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Product>> CreateProduct ([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest("Invalid Product");
+
+            await _repository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Product>> UpdateProduct([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest("Invalid Product");
+
+            var updateProduct = await _repository.UpdateProduct(product);
+
+            return Ok(updateProduct);
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Product>> DeleteProduct(string id)
+        {
+            if (id is null)
+                return BadRequest("Invalid Product");
+
+            var deleteProduct = await _repository.DeleteProduct(id);
+
+            return Ok(deleteProduct);
+        }
+    }
+}
---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,61 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productColletion)
+        {
+            bool existProduct = productColletion.Find(p => true).Any();
+            if (!existProduct)
+                productColletion.InsertManyAsync(GetMyProducts());
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {   new Product()
+                {
+                    Id = "1",
+                    Name = "Iphone X",
+                    Description = "Teste 1",
+                    Image = "iphone.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "2",
+                    Name = "Samsung S20",
+                    Description = "Teste 2",
+                    Image = "samsung.png",
+                    Price = 750.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "3",
+                    Name = "TV 55 Samsung",
+                    Description = "Teste 3",
+                    Image = "tvsamsung.png",
+                    Price = 3550.00M,
+                    Category = "Smart TV"
+                },
+                new Product()
+                {
+                    Id = "4",
+                    Name = "Notebook Acer",
+                    Description = "Teste 4",
+                    Image = "note.png",
+                    Price = 1000.00M,
+                    Category = "Notebook"
+                }
+            };
+        }
+    }
+}
